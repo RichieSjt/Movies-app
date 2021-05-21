fbf267f6bf4e2ce4eea6adc91ace5ddf04c2eaf0
Pedro Rangel <42298267+PedroRangelP@users.noreply.github.com>
2021-05-21 01:20:01 -0500

    Conexión al cluster

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const MONGODB_URL = "mongodb://127.0.0.1:27017/netflix_db"
+const MONGODB_URL = "mongodb+srv://pedrorangelp:2X2MwifJC5HvvtFp@mongodb-cluster.u0629.mongodb.net/netflix_db"
 
 const connect = async () => {
     try {
